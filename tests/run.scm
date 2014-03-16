7624826ea03a93f779458d545f47b0aac013821b
David Krentzlin <github@lisp-unleashed.de>
2014-03-16 11:14:30 +0100

    updated tests to work around a limitation on windows

--- tests/run.scm
+++ tests/run.scm
@@ -2,12 +2,15 @@
 
 (test-begin "micro-benchmark")
 
-(parameterize ((current-test-epsilon 0.0001))
+(define epsilon (cond-expand ((or mingw32 cygwin) 0.0001) (else 0.0001)))
+(define sleep-time (cond-expand ((or mingw32 cygwin) 2) (else 1)))
+
+(parameterize ((current-test-epsilon epsilon))
   (test "benchmark-measure"
         1000000.0
-        (benchmark-measure (sleep 1)))
+        (benchmark-measure (sleep sleep-time)))
   (test-group "benchmark-run"
-              (let ((result (parameterize ((current-benchmark-iterations 3)) (benchmark-run (sleep 1)))))
+              (let ((result (parameterize ((current-benchmark-iterations 3)) (benchmark-run (sleep sleep-time)))))
                 (test "min"
                       1000000.0
                       (alist-ref 'min result))
