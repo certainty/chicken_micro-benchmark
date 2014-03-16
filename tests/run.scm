6acb53bd340f98fa9845642b76edd2878dc52f4b
David Krentzlin <github@lisp-unleashed.de>
2014-03-16 11:17:29 +0100

    updated tests

--- tests/run.scm
+++ tests/run.scm
@@ -2,20 +2,20 @@
 
 (test-begin "micro-benchmark")
 
-(define epsilon (cond-expand ((or mingw32 cygwin) 0.001) (else 0.0001)))
+(define epsilon    (cond-expand ((or mingw32 cygwin) 0.001) (else 0.0001)))
 (define sleep-time (cond-expand ((or mingw32 cygwin) 2) (else 1)))
 
 (parameterize ((current-test-epsilon epsilon))
   (test "benchmark-measure"
-        1000000.0
+        (* sleep-time 1000000.0)
         (benchmark-measure (sleep sleep-time)))
   (test-group "benchmark-run"
               (let ((result (parameterize ((current-benchmark-iterations 3)) (benchmark-run (sleep sleep-time)))))
                 (test "min"
-                      1000000.0
+                      (* sleep-time 1000000.0)
                       (alist-ref 'min result))
                 (test "max"
-                      1000000.0
+                      (* sleep-time 1000000.0)
                       (alist-ref 'max result))
                 (test "runtimes"
                       3
