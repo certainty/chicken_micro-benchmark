973a42e61c293883af0934b713b5649a221bd1af
certainty <certainty@localhost>
2014-05-17 11:58:02 +0200

    added guard to current-benchmark-iterations. Refactored sample-standard-deviation

--- micro-benchmark.scm
+++ micro-benchmark.scm
@@ -2,7 +2,7 @@
   (benchmark-measure benchmark-run %gettime/microsecs current-benchmark-iterations generate-statistics)
 
   (import chicken scheme foreign)
-  (use (only srfi-1 list-tabulate fold) (only data-structures alist-ref))
+  (use (only srfi-1 fold list-tabulate))
 
   (cond-expand
    ((or netbsd openbsd freebsd linux)
@@ -23,7 +23,10 @@
              (error "Could not retrieve time reliably"))
          (- stop start)))))
 
-  (define current-benchmark-iterations (make-parameter 100))
+  (define current-benchmark-iterations (make-parameter 100 (lambda (x)
+                                                             (if (< x 2)
+                                                                 (error "You need at least 2 benchmark iterations")
+                                                                 x))))
 
   ;; run the given procedure n times and return statistics about the runtime
   ;; returns an alist with statistics
@@ -32,7 +35,7 @@
       ((_ ?code)
        (benchmark-run (current-benchmark-iterations) ?code))
       ((_ ?iterations ?code)
-       (let ((runtimes (map (lambda _ (benchmark-measure ?code)) (iota ?iterations))))
+       (let ((runtimes (list-tabulate ?iterations (lambda _ (benchmark-measure ?code)))))
          (generate-statistics runtimes)))))
 
 
@@ -48,11 +51,18 @@
   (define (mean values)
     (/ (apply + values) (length values)))
 
-  (define (sample-standard-deviation values #!optional (m (mean values)))
-    (sqrt (/ (fold (lambda (elt acc)
-                     (+ acc (expt (- elt m) 2)))
-                   0 values)
-             (- (length values) 1))))
+  (define (sample-variance values #!optional (m (mean values)))
+    (let ((amount (length values)))
+      (unless (> amount 1)
+        (error "Can not compute sample variance of less than 2 values"))
 
+      (/ (fold (lambda (elt acc)
+                 (+ acc (expt (- elt m) 2)))
+               0
+               values)
+         (- amount 1))))
+
+  (define (sample-standard-deviation values #!optional (m (mean values)))
+    (sqrt (sample-variance values m)))
 
   )
