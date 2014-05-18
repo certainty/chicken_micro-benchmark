61f978cba9f7b88c500811e986230df11a5830f1
certainty <certainty@localhost>
2014-05-18 11:36:26 +0200

    added benchmark-ips (inspired by:  https://github.com/evanphx/benchmark-ips )

--- micro-benchmark.scm
+++ micro-benchmark.scm
@@ -1,5 +1,5 @@
 (module micro-benchmark
-  (benchmark-measure benchmark-run %gettime/microsecs current-benchmark-iterations generate-statistics)
+  (benchmark-measure benchmark-run %gettime/microsecs current-benchmark-iterations generate-statistics (benchmark-ips benchmark-ips*))
 
   (import chicken scheme foreign)
   (use (only srfi-1 fold list-tabulate))
@@ -64,5 +64,56 @@
       (if (<= 0 sample-size 1)
           0.0
           (sqrt (sample-variance values m)))))
+
+  ;; benchmarking that tries to find out how many times your code can run in a given
+  ;; amount of time. Idea taken from: https://github.com/evanphx/benchmark-ips
+  ;; the idea is to find out how many iterations we can do in a certain (small) amount
+  ;; of time and then tell how much iterations we can make for a fixed timeframe.
+  ;; This is for cases where you don't want to guess the amount of iterations
+  ;; that are needed to produce values that you can work with
+  (define-syntax benchmark-ips
+    (syntax-rules ()
+      ((_ ?code)
+       (benchmark-ips 5 ?code))
+      ((_ ?seconds ?code)
+       (benchmark-ips* (lambda () ?code) ?seconds))))
+
+  (define-syntax dotimes
+    (syntax-rules ()
+      ((_ ?n ?code ...)
+       (let loop ((n ?n))
+         (when (positive? n)
+           ?code ...
+           (loop (fx- n 1)))))))
+
+  (define (iterations-per-100ms thunk limit)
+    (let ((before (current-milliseconds))
+          (threshold (+ limit (current-milliseconds))))
+      (let loop ((iterations 0))
+        (cond
+         ((< (current-milliseconds) threshold)
+          (thunk)
+          (loop (fx+ 1 iterations)))
+         (else
+          (let* ((after (current-milliseconds))
+                 (total-time (- after before))
+                 (per-100ms  (inexact->exact (round (/ (* iterations 100) total-time)))))
+            (cons (max 1 per-100ms) total-time)))))))
+
+  (define (benchmark-ips* thunk #!optional (time 5) (warmup 2))
+    (let* ((per-100ms (car (iterations-per-100ms thunk (* 1000 warmup))))
+          (threshold  (+ (* time 1000) (current-milliseconds))))
+      (let loop ((iterations 0) (timings (list)))
+        (cond
+         ((< (current-milliseconds) threshold)
+          (let ((before (current-milliseconds))
+                (_ (dotimes per-100ms (thunk)))
+                (after  (current-milliseconds)))
+            (loop (fx+ iterations per-100ms) (cons (- after before) timings))))
+         (else
+          (let ((all-ips (map (lambda (i) (/ per-100ms (/ i 1000.0))) timings)))
+            (generate-statistics all-ips)))))))
+
+
 
   )
